e8b38fb9d50d942435fe5bfa5a5117fdb890f216
Disha Kothari <dishakothari1198@gmail.com>
2017-06-04 15:40:08 +0530

    first commit

---
+++ csi1.py
@@ -0,0 +1,41 @@
+import time
+start_time = time.clock()
+
+import string
+
+rack=(raw_input()).upper()
+r=rack
+l=len(rack)
+score=0
+answer={}
+
+scores = {"A": 1, "C": 3, "B": 3, "E": 1, "D": 2, "G": 2,
+         "F": 4, "I": 1, "H": 4, "K": 5, "J": 8, "M": 3,
+         "L": 1, "O": 1, "N": 1, "Q": 10, "P": 3, "S": 1,
+         "R": 1, "U": 1, "T": 1, "W": 4, "V": 4, "Y": 4,
+         "X": 8, "Z": 10}
+         
+fp=open("sowpods.txt","r")					#opening and reading the file
+for line in fp.readlines():
+	word=line.strip()					#taking each word
+	for i in word:						#checking each letter in word
+		for a in r:					#each letter in rack
+			if i==a:
+				r= string.replace(r,a,"", 1)	#deleting the element once occured
+				break
+		else:
+			break
+	else:
+		for s in word:					#to calculate the score
+			score+=scores[s]
+		answer.update({word:score})			#updating the answer dictionary
+		score=0						
+	r=rack
+fp.close()							#closing the file
+print "\nAND YOU GO\n"
+for key in sorted(answer):					#your final answer
+	print answer[key], key 
+	
+
+print time.clock() - start_time, "seconds"
+
